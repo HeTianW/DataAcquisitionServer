ac901a013431e5c9d31f3d6cc881af5457fda163
gorgor <15236205392@163.com>
2020-06-06 22:55:24 +0800

    Netty服务端

--- src/main/java/com/netty/NettyServerHandler.java
+++ src/main/java/com/netty/NettyServerHandler.java
@@ -3,7 +3,7 @@ package com.netty;
 import com.netty.codec.StudentPOJO;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 /**
@@ -11,33 +11,17 @@ import io.netty.util.CharsetUtil;
  * 1、我们自定义一个Handler，需要继承netty绑定好的某个HandlerAdapter（规范）
  * 2、这时我们自定义一个Handler
  */
-public class NettyServerHandler extends SimpleChannelInboundHandler<StudentPOJO.Student> {
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
+
     public void channelRead0(ChannelHandlerContext ctx, StudentPOJO.Student msg) throws Exception {
 
         //读取从客户端发送的StudentPojo.Student
 
 
-        System.out.println("客户端发送的数据 id=" + msg.getId() + " 名字=" + msg.getName());
+        System.out.println(msg);
     }
 
-
-    //    /**
-//     * ChannelHandlerContext:读取客户端发送的消息
-//     * @param ctx:上下对象，含有管道pipeline，通道channel，地址
-//     * @param msg：客户端发送的数据，默认Object
-//     * @throws Exception
-//     */
-//    @Override
-//    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-//
-//        //读取从客户端发送的StudentPOJO.Student
-//
-//        StudentPOJO.Student student = (StudentPOJO.Student)msg;
-//
-//        System.out.println("客户端发送的数据 id="+ student.getId() + "名字="+ student.getName());
-//    }
 
     //数据读取完毕
     @Override
@@ -46,7 +30,7 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<StudentPOJO.
         //writeAndFlash ： write+flash
         //将数据写入到缓存并刷新
         //一般我们对这个发送的数据进行编码
-        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello,客户端~汪1", CharsetUtil.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello,客户端~", CharsetUtil.UTF_8));
     }
 
     //处理异常，一般是需要关闭通道
