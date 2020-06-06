6bd720d86546f31a3cce319448281301a72d941e
gorgor <15236205392@163.com>
2020-06-06 22:13:11 +0800

    删除无用代码

---
+++ src/main/java/com/netty/NettyClientHandler.java
@@ -0,0 +1,35 @@
+package com.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.util.Random;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    //当通道就绪时就会触发该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        //随机的发送Student 或者 Worker对象
+        int random = new Random().nextInt(3);
+
+    }
+
+    //当通道有读取事件时，会触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址："+ ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
